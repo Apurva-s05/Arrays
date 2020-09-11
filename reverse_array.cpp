cd2f0ffa4335ac4c619768f263ecc2be79d4fb8d
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-09-11 15:23:43 +0530

    Update reverse_array.cpp

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -12,13 +12,15 @@ int main() {
 		cin>>a[i];
 	}
 
+	cout<<"Normal:"<<endl;
 	for(int i=0;i<n;i++){
-		cout<<" "<<a[i];        //array with elements entered by user
+		cout<<" "<<a[i];      //array with elements enter by user
 	}
     cout<<endl;
 
+	cout<<"Reversed:"<<endl;
 	for(int i=n-1;i>=0;i--){
-		cout<<" "<<a[i];        //Reverse array
+		cout<<" "<<a[i];      //Reverse array
 	}
     cout<<endl;
 
